cb4ebeb05a3c8933505f2480df4a24b23f975775
crisjua <jua22478@uvg.edu.gt>
2026-07-09 17:40:35 -0600

    Create main.c

---
+++ Laboratorio1/Laboratorio1/Laboratorio1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Laboratorio1.c
+ *
+ * Created: 9/7/2026 17:39:21
+ * Author : crist
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
