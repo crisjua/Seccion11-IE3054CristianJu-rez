16f7132108f4fbc622dbd61690eadae6f06b5a03
crisjua <jua22478@uvg.edu.gt>
2026-07-16 09:38:37 -0600

    Create Display.h

---
+++ Laboratorio1/PostLab1/PostLab1/Display.h
@@ -0,0 +1,15 @@
+/*
+ * Display.h
+ *
+ * Created: 9/7/2026 17:40:10
+ *  Author: crist
+ */ 
+
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+void initDislayPorts(void);
+void display(uint8_t dig);
+
+#endif /* DISPLAY_H_ */
